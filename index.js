f2078f6560be98fa1f8a34b99e02c50b1d5fa569
Alex6n <hheikal.work@gmail.com>
2023-10-20 11:19:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const prompt = require('prompt-sync')();
 
 function mCalc() {
     let P = parseFloat(prompt('Enter the loan amount: '));
-    let r = parseFloat(prompt('Enter the monthly interest rate (in percentage): '); // monthly intrest rate directly instead
-    //let r = (parseFloat(prompt('Enter the annual interest rate (in percentage): ')) / 12) / 100;
+    let r = (parseFloat(prompt('Enter the annual interest rate (in percentage): ')) / 12) / 100;
     let n = parseFloat(prompt('Enter the number of years: ')) * 12;
     let M = (P * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
     
